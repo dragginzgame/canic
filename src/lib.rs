2a16c11e62a2b97bd20d9f5ce4644656ec1bc143
borovan <84897664+borovan@users.noreply.github.com>
2025-05-08 13:47:34 +0200

    added helper functionf rom mimic

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
 pub mod wasm;
+
+use sha2::{Digest, Sha256};
+
+// get_wasm_hash
+#[must_use]
+pub fn get_wasm_hash(bytes: &[u8]) -> Vec<u8> {
+    let mut hasher = Sha256::new();
+    hasher.update(bytes);
+
+    hasher.finalize().to_vec()
+}
