1cc17e6e627d206a60bf10cec2b8b41b22aea4b8
borovan <84897664+borovan@users.noreply.github.com>
2025-05-10 13:58:05 +0200

    fixed macro

--- src/macros.rs
+++ src/macros.rs
@@ -3,7 +3,7 @@ macro_rules! impl_storable_bounded {
     ($ident:ident, $max_size:expr, $is_fixed_size:expr) => {
         impl ::ic_stable_structures::storable::Storable for $ident {
             fn to_bytes(&self) -> ::std::borrow::Cow<[u8]> {
-                ::std::borrow::Cow::Owned(::mimic::db::serialize(self).unwrap())
+                ::std::borrow::Cow::Owned(::icu::serialize::serialize(self).unwrap())
             }
 
             fn from_bytes(bytes: ::std::borrow::Cow<[u8]>) -> Self {
@@ -24,7 +24,7 @@ macro_rules! impl_storable_unbounded {
     ($ident:ident) => {
         impl ::ic_stable_structures::storable::Storable for $ident {
             fn to_bytes(&self) -> ::std::borrow::Cow<[u8]> {
-                ::std::borrow::Cow::Owned(::mimic::db::serialize(self).unwrap())
+                ::std::borrow::Cow::Owned(::icu::serialize::serialize(self).unwrap())
             }
 
             fn from_bytes(bytes: ::std::borrow::Cow<[u8]>) -> Self {
