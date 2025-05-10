06ad3fa588809764f2c8b637a466463f69ba9fb6
borovan <84897664+borovan@users.noreply.github.com>
2025-05-10 15:39:41 +0200

    public states

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -3,10 +3,10 @@ pub mod canister_state;
 pub mod child_index;
 pub mod subnet_index;
 
-pub use app_state::{AppCommand, AppMode, AppState, AppStateData};
-pub use canister_state::{CanisterState, CanisterStateData, CanisterStateError};
-pub use child_index::{ChildIndex, ChildIndexData, ChildIndexError};
-pub use subnet_index::{SubnetIndex, SubnetIndexData, SubnetIndexError};
+pub use app_state::{APP_STATE, AppCommand, AppMode, AppState, AppStateData};
+pub use canister_state::{CANISTER_STATE, CanisterState, CanisterStateData, CanisterStateError};
+pub use child_index::{CHILD_INDEX, ChildIndex, ChildIndexData, ChildIndexError};
+pub use subnet_index::{SUBNET_INDEX, SubnetIndex, SubnetIndexData, SubnetIndexError};
 
 use crate::memory_manager;
 
