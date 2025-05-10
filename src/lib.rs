3e7118dadb0f0fdd293e4acd1e679538e7577499
borovan <84897664+borovan@users.noreply.github.com>
2025-05-10 13:03:49 +0200

    added Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
 pub mod cycles;
 pub mod helper;
 pub mod wasm;
+
+use candid::CandidType;
+use serde::{Deserialize, Serialize};
+use thiserror::Error as ThisError;
+
+///
+/// Error
+///
+
+#[derive(CandidType, Debug, Serialize, Deserialize, ThisError)]
+pub enum Error {
+    #[error(transparent)]
+    WasmError(#[from] wasm::WasmError),
+}
