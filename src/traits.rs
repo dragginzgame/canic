ae66e2dfd406366e98e4636c334883c23a6da3c7
borovan <84897664+borovan@users.noreply.github.com>
2025-05-12 14:55:46 +0200

    dynamic trait

--- src/traits.rs
+++ src/traits.rs
@@ -1,8 +1,10 @@
+use std::fmt::Display;
+
 ///
 /// Canister
 ///
 
-pub trait Canister: Sized + ToString {
+pub trait Canister: Display {
     fn path(&self) -> String;
     fn is_sharded(&self) -> bool;
 }
