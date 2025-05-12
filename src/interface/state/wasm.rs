9ed09b8cdad2a6bb85a890c71c25b812f98e4f90
borovan <84897664+borovan@users.noreply.github.com>
2025-05-12 12:20:37 +0200

    more interface glue code

--- src/interface/state/wasm.rs
+++ src/interface/state/wasm.rs
@@ -3,6 +3,20 @@ use crate::{
     state::{StateError, wasm::WasmManager},
 };
 
+// add_wasm
+pub fn add_wasm<S: ToString>(path: S, wasm: &'static [u8]) -> Result<(), Error> {
+    WasmManager::add_wasm(path, wasm).map_err(StateError::WasmError)?;
+
+    Ok(())
+}
+
+// add_wasms
+pub fn add_wasms<S: ToString>(wasms: &[(S, &'static [u8])]) -> Result<(), Error> {
+    WasmManager::add_wasms(wasms).map_err(StateError::WasmError)?;
+
+    Ok(())
+}
+
 // get_wasm
 pub fn get_wasm<S: ToString>(path: S) -> Result<&'static [u8], Error> {
     let wasm = WasmManager::get_wasm(path).map_err(StateError::WasmError)?;
