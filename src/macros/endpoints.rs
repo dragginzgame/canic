8affc239a318ffe9b5985d96074577faf3dbf7e0
borovan <84897664+borovan@users.noreply.github.com>
2025-05-12 13:48:11 +0200

    oops

--- src/macros/endpoints.rs
+++ src/macros/endpoints.rs
@@ -40,7 +40,7 @@ macro_rules! icu_endpoints {
             //           allow_any(vec![Auth::Controller]).await?;
 
             // send a request for each matching canister
-            for (child_id, path) in ::icu::interface::core::state::child_index::get_data() {
+            for (child_id, path) in ::icu::interface::state::core::child_index::get_data() {
                 if canister_id.is_none() || canister_id == Some(child_id) {
                     let req = ::icu::interface::request::Request::new_canister_upgrade(
                         child_id,
