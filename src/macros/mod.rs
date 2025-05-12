99fc0a444995e042a5b9de623ec7c99687ef8f46
borovan <84897664+borovan@users.noreply.github.com>
2025-05-12 13:24:57 +0200

    init_async

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,47 +1,6 @@
 pub mod endpoints;
+pub mod start;
 pub mod state;
-
-/// icu_start
-#[macro_export]
-macro_rules! icu_start {
-    ($canister_path:path) => {
-        #[::icu::ic::init]
-        fn init(root_id: ::candid::Principal, parent_id: ::candid::Principal) {
-            use ::icu::interface::state::core::canister_state;
-
-            canister_state::set_root_id(root_id).unwrap();
-            canister_state::set_parent_id(parent_id).unwrap();
-            canister_state::set_path($canister_path).unwrap();
-
-            log!(Log::Info, "init: {}", $canister_path);
-
-            _init();
-        }
-
-        ::icu::icu_endpoints!();
-    };
-}
-
-/// icu_start_root
-#[macro_export]
-macro_rules! icu_start_root {
-    ($canister_path:path) => {
-        #[::icu::ic::init]
-        fn init() {
-            use ::icu::interface::state::core::canister_state;
-
-            canister_state::set_root_id(::icu::ic::api::canister_self()).unwrap();
-            canister_state::set_path($canister_path).unwrap();
-
-            log!(Log::Info, "init: {} (root)", $canister_path);
-
-            _init();
-        }
-
-        ::icu::icu_endpoints_root!();
-        ::icu::icu_endpoints!();
-    };
-}
 
 // log
 #[macro_export]
