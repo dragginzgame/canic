436ac147ac7c06469a4ab60fd3394111b88513e4
borovan <84897664+borovan@users.noreply.github.com>
2025-05-12 13:05:45 +0200

    fix macro

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -4,18 +4,16 @@ pub mod state;
 /// icu_start
 #[macro_export]
 macro_rules! icu_start {
-    ($canister_path:expr) => {
+    ($canister_path:path) => {
         #[::icu::ic::init]
         fn init(root_id: Option<Principal>, parent_id: Option<Principal>) {
             use ::icu::interface::state::core::canister_state;
 
             canister_state::set_root_id(root_id).unwrap();
             canister_state::set_parent_id(parent_id).unwrap();
+            canister_state::set_type($canister_type).unwrap();
 
             log!(Log::Info, "init: {}", $canister_type);
-
-            // type
-            canister_state::set_type($canister_type).unwrap();
 
             _init();
         };
@@ -27,17 +25,15 @@ macro_rules! icu_start {
 /// icu_start_root
 #[macro_export]
 macro_rules! icu_start_root {
-    ($canister_path:expr) => {
+    ($canister_path:path) => {
         #[::icu::ic::init]
         fn init() {
             use ::icu::interface::state::core::canister_state;
 
             canister_state::set_root_id(canister_self()).unwrap();
+            canister_state::set_type($canister_type).unwrap();
 
             log!(Log::Info, "init: {} (root)",);
-
-            // type
-            canister_state::set_type($canister_type).unwrap();
 
             _init();
         };
