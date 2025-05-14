ae9cd92ee32007b04bba14180cbcffc98b971957
borovan <84897664+borovan@users.noreply.github.com>
2025-05-14 13:59:48 +0200

    better memory logging

--- src/macros/memory.rs
+++ src/macros/memory.rs
@@ -3,23 +3,36 @@ macro_rules! icu_register_memory {
     ($ty:ty, $id:expr, $init:expr) => {{
         let path = stringify!($ty).to_string();
 
-        // check the registry
+        // check the registry with logging
         $crate::memory::MEMORY_REGISTRY.with_borrow_mut(|reg| {
-            reg.register($id, $crate::memory::registry::RegistryEntry { path })
-                .unwrap();
+            let result = reg.register(
+                $id,
+                $crate::memory::registry::RegistryEntry { path: path.clone() },
+            );
+
+            if let Err(ref err) = result {
+                $crate::log!(
+                    $crate::Log::Error,
+                    "❌ icu_register_memory failed for {} @ {}: {}",
+                    path,
+                    $id,
+                    err
+                );
+            } else {
+                $crate::log!(
+                    $crate::Log::Info,
+                    "✅ icu_register_memory registered {} @ {}",
+                    path,
+                    $id
+                );
+            }
+
+            result.unwrap()
         });
 
         // acquire memory_id
         let mem = $crate::memory::MEMORY_MANAGER
             .with_borrow_mut(|mgr| mgr.get($crate::ic::structures::memory::MemoryId::new($id)));
-
-        // log
-        $crate::log!(
-            $crate::Log::Info,
-            "icu_register_memory {} {}",
-            stringify!($ty),
-            $id
-        );
 
         // init
         $init(mem)
