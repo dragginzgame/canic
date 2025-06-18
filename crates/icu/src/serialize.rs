9493af4b64d3f43cda9573a15c6cb278633dfb1a
borovan <84897664+borovan@users.noreply.github.com>
2025-06-18 12:20:44 +0200

    back to ciborium for now

--- crates/icu/src/serialize.rs
+++ crates/icu/src/serialize.rs
@@ -1,11 +1,12 @@
 use candid::CandidType;
+use ciborium::{Value, de::from_reader, ser::into_writer};
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
 use std::fmt::Debug;
 use thiserror::Error as ThisError;
 
 ///
 /// Serialize/Deserialize
-/// specific about the type of serializer it uses
+/// forces use of cbor (ciborium)
 ///
 
 ///
@@ -21,18 +22,31 @@ pub enum SerializeError {
     Deserialize(String),
 }
 
-/// Serialize using `serde_cbor` (non-canonical)
-pub fn serialize<T>(value: &T) -> Result<Vec<u8>, SerializeError>
+// serialize
+pub fn serialize<T>(ty: &T) -> Result<Vec<u8>, SerializeError>
 where
     T: Serialize,
 {
-    serde_cbor::to_vec(value).map_err(|e| SerializeError::Serialize(e.to_string()))
+    let mut writer = Vec::<u8>::new();
+    into_writer(ty, &mut writer).map_err(|e| SerializeError::Serialize(e.to_string()))?;
+
+    Ok(writer)
 }
 
-/// Deserialize using `serde_cbor`
+// deserialize
 pub fn deserialize<T>(bytes: &[u8]) -> Result<T, SerializeError>
 where
     T: DeserializeOwned,
 {
-    serde_cbor::from_slice(bytes).map_err(|e| SerializeError::Deserialize(e.to_string()))
+    from_reader(bytes).map_err(|e| {
+        // attempt to deserialize into a more generic Value for debugging
+        match from_reader::<Value, _>(bytes) {
+            Ok(value) => {
+                SerializeError::Deserialize(format!("failed to deserialize: {e} ({value:?})"))
+            }
+            Err(debug_error) => SerializeError::Deserialize(format!(
+                "failed to deserialize: {e}. DEBUG FAILED {debug_error}"
+            )),
+        }
+    })
 }
