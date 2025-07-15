2d810d8153fb6b0d23c332f7df7903b29efc6570
borovan <84897664+borovan@users.noreply.github.com>
2025-07-15 12:43:34 +0200

    back in prelude

--- crates/icu/src/lib.rs
+++ crates/icu/src/lib.rs
@@ -25,8 +25,13 @@ pub mod prelude {
         Error as IcuError, Log, auth_require_all, auth_require_any,
         guard::{guard_query, guard_update},
         ic::{
-            api::msg_caller, candid::CandidType, export_candid, init, principal::Principal, query,
-            update,
+            api::msg_caller,
+            candid::CandidType,
+            export_candid,
+            icrc_ledger_types::icrc1::account::{Account, Subaccount},
+            init,
+            principal::Principal,
+            query, update,
         },
         icu_register_memory, icu_start, icu_start_root, log, perf, perf_start,
     };
