f1f2b1e4d691c3f471476cc65953dac83d07d2d6
borovan <84897664+borovan@users.noreply.github.com>
2025-07-29 14:12:11 +0200

    added the whole ircrc21 spec

--- crates/icu/src/state/icrc/icrc_21.rs
+++ crates/icu/src/state/icrc/icrc_21.rs
@@ -1,6 +1,6 @@
 use candid::CandidType;
 use derive_more::{Deref, DerefMut};
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use std::{cell::RefCell, collections::HashMap};
 
 //
@@ -12,38 +12,127 @@ thread_local! {
 }
 
 ///
-/// Data Structures
+/// ConsentHandlerFn
+/// this is what the user has to pass into icu
 ///
 
 pub type Icrc21ConsentHandlerFn = fn(
-    arg: Vec<u8>,
-    prefs: Icrc21ConsentPreferences,
-) -> Result<Option<Icrc21ConsentMessage>, String>;
+    request: Icrc21ConsentMessageRequest,
+) -> Result<Option<Icrc21ConsentMessageResponse>, String>;
 
-#[derive(Clone, Debug, CandidType, Deserialize, Serialize)]
-pub struct Icrc21ConsentMessage {
-    pub consent_message: String,
-    pub language: String,
+///
+
+///
+/// Icrc21 ConsentMessage
+///
+
+#[derive(CandidType, Deserialize)]
+pub enum Icrc21ConsentMessage {
+    GenericDisplayMessage(String),
+    FieldsDisplayMessage {
+        intent: String,
+        fields: Vec<(String, Icrc21Value)>,
+    },
 }
+
+#[derive(CandidType, Deserialize, Clone)]
+pub struct Icrc21ConsentMessageSpec {
+    pub metadata: Icrc21ConsentMessageMetadata,
+    pub device_spec: Option<Icrc21DeviceSpec>,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct Icrc21ConsentMessageRequest {
+    pub method: String,
+    pub arg: Vec<u8>,
+    pub user_preferences: Icrc21ConsentPreferences,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum Icrc21ConsentMessageResponse {
+    Ok(Icrc21ConsentInfo),
+    Err(Icrc21Error),
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct Icrc21ConsentInfo {
+    pub consent_message: Icrc21ConsentMessage,
+    pub metadata: Icrc21ConsentMessageMetadata,
+}
+
+#[derive(CandidType, Deserialize, Clone)]
+pub struct Icrc21ConsentMessageMetadata {
+    pub language: String,
+    pub utc_offset_minutes: Option<i16>,
+}
+
+///
+/// Icrc21ConsertPreferences
+///
 
 #[derive(CandidType, Deserialize, Clone)]
 pub struct Icrc21ConsentPreferences {
     pub language: String,
 }
 
-#[derive(CandidType, Serialize)]
-pub enum Icrc21ConsentMessageResponse {
-    Ok {
-        consent_message: String,
-        language: String,
-    },
+#[derive(CandidType, Deserialize, Clone)]
+pub enum Icrc21DeviceSpec {
+    GenericDisplay,
+    FieldsDisplay,
+}
+
+///
+/// Icrc21 Values
+///
+
+#[derive(CandidType, Deserialize)]
+pub enum Icrc21Value {
+    TokenAmount(Icrc21TokenAmount),
+    TimestampSeconds(Icrc21TimestampSeconds),
+    DurationSeconds(Icrc21DurationSeconds),
+    Text(Icrc21TextValue),
 }
 
 #[derive(CandidType, Deserialize)]
-pub struct Icrc21ConsentMessageRequest {
-    pub method: String,
-    pub arg: Vec<u8>, // Candid-encoded
-    pub consent_preferences: Icrc21ConsentPreferences,
+pub struct Icrc21TokenAmount {
+    pub decimals: u8,
+    pub amount: u64,
+    pub symbol: String,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct Icrc21TimestampSeconds {
+    pub amount: u64,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct Icrc21DurationSeconds {
+    pub amount: u64,
+}
+
+#[derive(CandidType, Deserialize)]
+pub struct Icrc21TextValue {
+    pub content: String,
+}
+
+///
+/// Icrc21Error
+///
+
+#[derive(CandidType, Deserialize)]
+pub struct Icrc21ErrorInfo {
+    pub description: String,
+}
+
+#[derive(CandidType, Deserialize)]
+pub enum Icrc21Error {
+    UnsupportedCanisterCall(Icrc21ErrorInfo),
+    ConsentMessageUnavailable(Icrc21ErrorInfo),
+    InsufficientPayment(Icrc21ErrorInfo),
+    GenericError {
+        error_code: u64,
+        description: String,
+    },
 }
 
 ///
@@ -71,27 +160,26 @@ impl Icrc21Registry {
     #[must_use]
     pub fn consent_message(req: Icrc21ConsentMessageRequest) -> Icrc21ConsentMessageResponse {
         match Self::get_handler(&req.method) {
-            Some(handler) => match handler(req.arg.clone(), req.consent_preferences.clone()) {
-                Ok(Some(msg)) => Icrc21ConsentMessageResponse::Ok {
-                    consent_message: msg.consent_message,
-                    language: msg.language,
-                },
+            Some(handler) => match handler(req) {
+                Ok(Some(response)) => response,
 
-                Ok(None) => Icrc21ConsentMessageResponse::Ok {
-                    consent_message: "No consent message available.".to_string(),
-                    language: "en-US".to_string(),
-                },
+                Ok(None) => Icrc21ConsentMessageResponse::Err(
+                    Icrc21Error::ConsentMessageUnavailable(Icrc21ErrorInfo {
+                        description: "No consent message available.".to_string(),
+                    }),
+                ),
 
-                Err(_) => Icrc21ConsentMessageResponse::Ok {
-                    consent_message: "Error generating consent message.".to_string(),
-                    language: "en-US".to_string(),
-                },
+                Err(desc) => Icrc21ConsentMessageResponse::Err(Icrc21Error::GenericError {
+                    error_code: 1,
+                    description: desc,
+                }),
             },
 
-            None => Icrc21ConsentMessageResponse::Ok {
-                consent_message: "No handler registered for method.".to_string(),
-                language: "en-US".to_string(),
-            },
+            None => Icrc21ConsentMessageResponse::Err(Icrc21Error::UnsupportedCanisterCall(
+                Icrc21ErrorInfo {
+                    description: "No handler registered for this method.".to_string(),
+                },
+            )),
         }
     }
 }
