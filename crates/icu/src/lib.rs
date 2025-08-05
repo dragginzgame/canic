f9231df88c8b74fd39dde252b9522bb8ca28a956
borovan <84897664+borovan@users.noreply.github.com>
2025-08-05 15:24:28 +0200

    more unneeded code gone

--- crates/icu/src/lib.rs
+++ crates/icu/src/lib.rs
@@ -15,7 +15,7 @@ pub mod export {
 pub use Error as IcuError;
 
 use candid::CandidType;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use thiserror::Error as ThisError;
 
 ///
@@ -85,71 +85,4 @@ pub enum Log {
     Info,
     Warn,
     Error,
-}
-
-///
-/// Cycles
-///
-
-#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
-pub struct Cycles(pub u128);
-
-impl From<u128> for Cycles {
-    fn from(n: u128) -> Self {
-        Self(n)
-    }
-}
-
-// parse_cycles
-// helper function to parse string with multiplier suffix
-pub fn parse_cycles(value: &str) -> Result<u128, String> {
-    let mut num_str = String::new();
-    let mut suffix_str = String::new();
-    let mut seen_dot = false;
-
-    for c in value.chars() {
-        if c.is_ascii_digit() || (c == '.' && !seen_dot) {
-            if c == '.' {
-                seen_dot = true;
-            }
-            num_str.push(c);
-        } else {
-            suffix_str.push(c);
-        }
-    }
-
-    let number: f64 = num_str.parse::<f64>().map_err(|e| e.to_string())?;
-
-    let multiplier = match suffix_str.as_str() {
-        "K" => 1_000_f64,
-        "M" => 1_000_000_f64,
-        "B" => 1_000_000_000_f64,
-        "T" => 1_000_000_000_000_f64,
-        "Q" => 1_000_000_000_000_000_f64,
-        _ => 1_f64,
-    };
-
-    #[allow(clippy::cast_sign_loss)]
-    #[allow(clippy::cast_possible_truncation)]
-    Ok((number * multiplier) as u128)
-}
-
-///
-/// Instructions
-///
-
-#[must_use]
-#[allow(clippy::cast_precision_loss)]
-pub fn format_instructions(n: u64) -> String {
-    if n >= 1_000_000_000_000 {
-        format!("{:.2}T", n as f64 / 1_000_000_000_000.0)
-    } else if n >= 1_000_000_000 {
-        format!("{:.2}B", n as f64 / 1_000_000_000.0)
-    } else if n >= 1_000_000 {
-        format!("{:.2}M", n as f64 / 1_000_000.0)
-    } else if n >= 1_000 {
-        format!("{:.2}K", n as f64 / 1_000.0)
-    } else {
-        format!("{n}")
-    }
 }
