5fb8259db5c5eee5a99d114c096cbc34e89f7fca
borovan <84897664+borovan@users.noreply.github.com>
2025-08-07 13:36:13 +0200

    update install script

--- scripts/app/install.sh
+++ scripts/app/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-dfx canister create --all -qq
-dfx build --all
+dfx canister create test -qq
+dfx build test
+dfx canister create root -qq
+dfx build root
+
 
 dfx ledger fabricate-cycles --canister root --cycles 9000000000000000
 dfx canister install root --mode=reinstall -y
