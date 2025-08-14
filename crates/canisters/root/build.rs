689b486bc94822f79f4eefc0ee7c815824a01b78
borovan <84897664+borovan@users.noreply.github.com>
2025-08-14 12:03:16 +0200

    please work

--- crates/canisters/root/build.rs
+++ crates/canisters/root/build.rs
@@ -2,11 +2,12 @@ fn main() -> std::io::Result<()> {
     icu::icu_build!("../icu.toml");
 
     // Tell rustc that `icu_github_ci` is a valid cfg to avoid warnings.
-    println!("cargo::rustc-check-cfg=cfg(icu_github_ci)");
+    println!("cargo::rustc-check-cfg=cfg(github_ci)");
 
-    // Auto-enable the cfg when running under GitHub Actions.
+    // Check the GitHub Actions env var
     if std::env::var("GITHUB_ACTIONS").as_deref() == Ok("true") {
-        println!("cargo:rustc-cfg=icu_github_ci");
+        // Emit a custom cfg flag to the compiler
+        println!("cargo:rustc-cfg=github_ci");
     }
 
     Ok(())
