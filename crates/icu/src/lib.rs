b75ec4f75203c87c7a4128647a8edf7d7f867960
borovan <84897664+borovan@users.noreply.github.com>
2025-08-15 09:56:59 +0200

    VERSION

--- crates/icu/src/lib.rs
+++ crates/icu/src/lib.rs
@@ -21,11 +21,10 @@ use serde::Deserialize;
 use thiserror::Error as ThisError;
 
 ///
-/// CRATE
+/// Crate Version
 ///
 
-pub const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
-pub const CRATE_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 ///
 /// Prelude
