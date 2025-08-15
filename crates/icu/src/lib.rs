abb89e4d2e17f87d82fdc1aa6d1d3f23be3183b9
borovan <84897664+borovan@users.noreply.github.com>
2025-08-15 09:49:29 +0200

    added crate name and version

--- crates/icu/src/lib.rs
+++ crates/icu/src/lib.rs
@@ -21,6 +21,13 @@ use serde::Deserialize;
 use thiserror::Error as ThisError;
 
 ///
+/// CRATE
+///
+
+pub const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
+pub const CRATE_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+///
 /// Prelude
 ///
 
