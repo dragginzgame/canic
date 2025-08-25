80ca69cae053f9177fe3c11774b4a4c5480d3e6c
borovan <84897664+borovan@users.noreply.github.com>
2025-08-25 19:01:19 +0200

    forgot consentinfo

--- crates/icu/src/spec/icrc/icrc21.rs
+++ crates/icu/src/spec/icrc/icrc21.rs
@@ -1,7 +1,7 @@
 pub use crate::cdk::icrc_ledger_types::icrc21::{
     errors::{ErrorInfo, Icrc21Error},
     requests::ConsentMessageRequest,
-    responses::ConsentMessage,
+    responses::{ConsentInfo, ConsentMessage},
 };
 
 pub type ConsentMessageResponse = Result<ConsentMessage, Icrc21Error>;
