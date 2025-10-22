cbb55aa85d864b6e640cffba2337feb3da850817
borovan <84897664+borovan@users.noreply.github.com>
2025-10-22 12:34:28 +0200

    removed icrc-ledger-types

--- crates/canic/src/spec/icrc/icrc2.rs
+++ crates/canic/src/spec/icrc/icrc2.rs
@@ -1,5 +1,27 @@
-//!
-//! Re-export of ICRC-2 allowance arguments to keep naming consistent.
-//!
+use crate::types::Account;
+use candid::{CandidType, Nat};
+use serde::{Deserialize, Serialize};
 
-pub use icrc_ledger_types::icrc2::allowance::AllowanceArgs;
+///
+/// icrc-ledger-types has a conflict so we can just copy and paste here for now
+/// @todo
+///
+
+/// The arguments for the
+/// [ICRC-2 `allowance`](https://github.com/dfinity/ICRC-1/blob/main/standards/ICRC-2/README.md#icrc2_allowance)
+/// endpoint.
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct AllowanceArgs {
+    pub account: Account,
+    pub spender: Account,
+}
+
+/// The `Allowance` response type for the
+/// [ICRC-2 `allowance`](https://github.com/dfinity/ICRC-1/blob/main/standards/ICRC-2/README.md#icrc2_allowance)
+/// endpoint.
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+pub struct Allowance {
+    pub allowance: Nat,
+    #[serde(default)]
+    pub expires_at: Option<u64>,
+}
