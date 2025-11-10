e843500b78b959e135f5929891d0ab91a14709e2
borovan <84897664+borovan@users.noreply.github.com>
2025-11-10 13:15:56 +0100

    changed tactic to just install_args for now

--- crates/canic/src/test/pic.rs
+++ crates/canic/src/test/pic.rs
@@ -1,27 +1,16 @@
 use crate::{
-    Error,
-    memory::topology::SubnetIdentity,
-    ops::CanisterInitPayload,
-    types::{CanisterType, Principal, TC},
+    Error, memory::topology::SubnetIdentity, ops::CanisterInitPayload, types::CanisterType,
 };
-use pocket_ic::PocketIc;
 
-// create_canister
-pub fn create_canister(pic: &PocketIc, ty: CanisterType, wasm: &[u8]) -> Result<Principal, Error> {
-    // create canister and add cycles
-    let canister_id = pic.create_canister();
-    pic.add_cycles(canister_id, 5 * TC);
-
-    // encode the init args for install
-    let init = if ty.is_root() {
-        candid::encode_one(SubnetIdentity::Test).unwrap()
+// install_args
+// these vary depending on the canister type
+pub fn install_args(ty: CanisterType) -> Result<Vec<u8>, Error> {
+    let args = if ty.is_root() {
+        candid::encode_one(SubnetIdentity::Test)
     } else {
         let payload = CanisterInitPayload::empty();
-        candid::encode_args::<(CanisterInitPayload, Option<Vec<u8>>)>((payload, None)).unwrap()
-    };
+        candid::encode_args::<(CanisterInitPayload, Option<Vec<u8>>)>((payload, None))
+    }?;
 
-    // install
-    pic.install_canister(canister_id, wasm.to_vec(), init, None);
-
-    Ok(canister_id)
+    Ok(args)
 }
