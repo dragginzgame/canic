cfecfcc9bd8c65fddfc3ed5045660699f6caa983
borovan <84897664+borovan@users.noreply.github.com>
2025-11-28 16:45:47 +0100

    scaling fix for planning create, removed todos, cleaned up icrc2

--- crates/canic/src/spec/icrc/icrc2.rs
+++ crates/canic/src/spec/icrc/icrc2.rs
@@ -1,27 +1,16 @@
+pub use icrc_ledger_types::icrc2::allowance::Allowance;
+
 use crate::types::Account;
-use candid::{CandidType, Nat};
+use candid::CandidType;
 use serde::{Deserialize, Serialize};
 
 ///
-/// icrc-ledger-types has a conflict so we can just copy and paste here for now
-/// @todo
+/// AllowanceArgs
+/// wrapped to use the canic Account
 ///
 
-/// The arguments for the
-/// [ICRC-2 `allowance`](https://github.com/dfinity/ICRC-1/blob/main/standards/ICRC-2/README.md#icrc2_allowance)
-/// endpoint.
 #[derive(CandidType, Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct AllowanceArgs {
     pub account: Account,
     pub spender: Account,
-}
-
-/// The `Allowance` response type for the
-/// [ICRC-2 `allowance`](https://github.com/dfinity/ICRC-1/blob/main/standards/ICRC-2/README.md#icrc2_allowance)
-/// endpoint.
-#[derive(CandidType, Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct Allowance {
-    pub allowance: Nat,
-    #[serde(default)]
-    pub expires_at: Option<u64>,
 }
