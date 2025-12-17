e2bff646c6d215eaf42af1cd0150b536004e5dba
borovan <84897664+borovan@users.noreply.github.com>
2025-12-17 14:36:44 +0100

    added bounded_wait to call

--- crates/canic-core/src/ops/ic/call.rs
+++ crates/canic-core/src/ops/ic/call.rs
@@ -16,9 +16,21 @@ use crate::{
 pub struct Call;
 
 impl Call {
+    #[must_use]
+    pub fn bounded_wait(canister_id: impl Into<Principal>, method: &str) -> IcCall<'_, '_> {
+        let canister_id: Principal = canister_id.into();
+
+        SystemMetrics::increment(SystemMetricKind::CanisterCall);
+        IccMetrics::increment(canister_id, method);
+
+        IcCall::bounded_wait(canister_id, method)
+    }
+
     /// Create a call builder that will be awaited without cycle limits.
     #[must_use]
-    pub fn unbounded_wait(canister_id: Principal, method: &str) -> IcCall<'_, '_> {
+    pub fn unbounded_wait(canister_id: impl Into<Principal>, method: &str) -> IcCall<'_, '_> {
+        let canister_id: Principal = canister_id.into();
+
         SystemMetrics::increment(SystemMetricKind::CanisterCall);
         IccMetrics::increment(canister_id, method);
 
