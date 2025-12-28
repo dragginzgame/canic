c630a67c99e919633ed720b8af9fa607f8d01523
borovan <84897664+borovan@users.noreply.github.com>
2025-12-28 16:16:50 +0100

    fixing directory resolvers

--- crates/canic-core/src/workflow/directory/builders.rs
+++ crates/canic-core/src/workflow/directory/builders.rs
@@ -12,19 +12,18 @@ pub struct RootAppDirectoryBuilder;
 impl RootAppDirectoryBuilder {
     #[must_use]
     pub fn build_from_registry() -> Vec<(CanisterRole, Principal)> {
-        let entries = SubnetRegistryOps::export_view();
-        let mut map = BTreeMap::new();
+        let entries = SubnetRegistryOps::export();
+        let mut map = BTreeMap::<CanisterRole, Principal>::new();
 
-        for entry in entries {
+        for (pid, entry) in entries {
             if policy::directory::is_app_directory_role(&entry.role) {
-                map.insert(entry.role.clone(), entry.pid);
+                map.insert(entry.role.clone(), pid);
             }
         }
 
         map.into_iter().collect()
     }
 }
-
 ///
 /// RootSubnetDirectoryBuilder
 ///
@@ -34,12 +33,12 @@ pub struct RootSubnetDirectoryBuilder;
 impl RootSubnetDirectoryBuilder {
     #[must_use]
     pub fn build_from_registry() -> Vec<(CanisterRole, Principal)> {
-        let entries = SubnetRegistryOps::export_view();
-        let mut map = BTreeMap::new();
+        let entries = SubnetRegistryOps::export();
+        let mut map = BTreeMap::<CanisterRole, Principal>::new();
 
-        for entry in entries {
+        for (pid, entry) in entries {
             if policy::directory::is_subnet_directory_role(&entry.role) {
-                map.insert(entry.role.clone(), entry.pid);
+                map.insert(entry.role.clone(), pid);
             }
         }
 
