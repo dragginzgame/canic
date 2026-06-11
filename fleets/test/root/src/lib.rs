70ed8a61914154d8911f5d11f5bbeaec125f3cc2
borovan <mouth@fart.com>
2026-06-11 17:58:33 +0200

    0.65.17

--- fleets/test/root/src/lib.rs
+++ fleets/test/root/src/lib.rs
@@ -1,13 +1,8 @@
 #![expect(clippy::unused_async)]
 
-use canic::api::auth::AuthApi;
-
 canic::start!();
 
-// Publish root auth material before the first live delegated-auth request path.
-async fn canic_setup() {
-    let _ = AuthApi::publish_root_auth_material().await;
-}
+async fn canic_setup() {}
 async fn canic_install() {}
 async fn canic_upgrade() {}
 
